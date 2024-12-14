921e81ad5a3bf9b6970b5ab0c3b5463dc6910989
chndnnn <chndn136@gmail.com>
2024-12-14 11:53:10 +0530

    Tailwind css setup

---
+++ src/Components/Input.jsx
@@ -0,0 +1,23 @@
+const Input = ({ name, width, onChange }) => {
+  return (
+    <>
+      <label htmlFor={name} className="relative block rounded-md">
+        <input
+          type="text"
+          id={name}
+          className={`peer border border-green-500 bg-transparent placeholder-transparent rounded ${
+            width ? `w-${width}` : "w-full"
+          }`}
+          placeholder={name}
+          onChange={onChange}
+        />
+
+        <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+          {name}
+        </span>
+      </label>
+    </>
+  );
+};
+
+export default Input;
