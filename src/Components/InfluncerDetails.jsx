a9acecbcc90a868ade38320fe7c32a3867a8e4e6
chndnnn <chndn136@gmail.com>
2024-12-25 21:44:03 +0530

    added Qulidy screen and changes in input

---
+++ src/Components/InfluncerDetails.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { fetchTableData } from "../Helper/Api";
+import Table from "./Table";
+import "./../index.css";
+import axios from "axios";
+
+const InfluncerDetails = () => {
+  const [tableData, setTableData] = useState([]);
+
+  async function getTest() {
+    try {
+      let data = await axios.post(
+        // "https://domestic-kate-wcclg-7ed3ef90.koyeb.app/lead/getAllLeads"
+        "https://domestic-kate-wcclg-7ed3ef90.koyeb.app/lead/getAllLeads"
+      );
+      setTableData(data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getTest();
+    //fetchData();
+  }, []);
+
+  async function fetchData() {
+    let data = await fetchTableData();
+    setTableData(data);
+  }
+  return (
+    <div className="w-full  gap-3 md:flex h-80 ">
+      <div className="md:w-[100%]  border rounded border-black ">
+        <Table data={tableData} name={"leadDetails"} />
+      </div>
+    </div>
+  );
+};
+
+export default InfluncerDetails;
--- src/Screens/Qulifying.jsx
+++ src/Screens/Qulifying.jsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Dropdown from "../Components/Dropdown";
 import Input from "../Components/Input";
-import LeadDetails from "../Components/LeadDetails";
 import { IoIosArrowDown } from "react-icons/io";
 import { FaAngleUp } from "react-icons/fa6";
+import InfluncerDetails from "../Components/InfluncerDetails";
 
-const Lead = () => {
+const Qualifying = () => {
+  let [hide, setHide] = useState(false);
   let [data, setdata] = useState([2, 3, 4, 5]);
   let [selectedValue, setSelectedValue] = useState();
-  let [hide, setHide] = useState(false);
 
   return (
     <div className="border">
       <div className="bg-blue-400">
-        <h1>Detail lead and enquiry</h1>
+        <h1>Qualify and Influncer screen</h1>
       </div>
       <div className="border p-2 ">
         <div
           className="bg-blue-500 px-2 mb-1 rounded flex items-center text-white cursor-pointer"
           onClick={() => setHide((prev) => !prev)}
         >
-          Create Lead
+          Qualifying details
           <span className="ml-auto">
             {hide ? <IoIosArrowDown /> : <FaAngleUp />}
           </span>
@@ -34,6 +34,7 @@ const Lead = () => {
               <div className="grid md:grid-cols-3 p-3 gap-4">
                 <Input
                   name={"Lead ID"}
+                  required={true}
                   onChange={(e) => setData(e.target.value)}
                 />
                 <Input
@@ -45,42 +46,31 @@ const Lead = () => {
                   name={"Project Name"}
                   onChange={(e) => setData(e.target.value)}
                 />
-                <Input
-                  name={"Project Location"}
-                  onChange={(e) => setData(e.target.value)}
-                />
-
-                <Input
-                  name={"Employee No"}
-                  onChange={(e) => setData(e.target.value)}
-                />
-                <Input
-                  name={"Sales Person"}
-                  onChange={(e) => setData(e.target.value)}
-                />
+                <Dropdown name={"Lead Source"} data={data} />
+                <Dropdown name={"Sub vertical"} data={data} />
+                <Dropdown name={"Type of account"} data={data} />
+                <Dropdown name={"Segment"} data={data} />
+                <Dropdown name={"Sub Segment"} data={data} />
+                <Dropdown name={"Lead expected closure date"} data={data} />
                 <Dropdown
-                  name={"Bussiness Unit"}
+                  name={"status"}
                   data={data}
                   onChange={(e) => setSelectedValue(e.target.value)}
                   value={selectedValue}
                 />
-                <Dropdown name={"Vertical"} data={data} />
-                <Dropdown name={"Region"} data={data} />
-                <Dropdown name={"Branch"} data={data} />
-                <Dropdown name={"Lead Type"} data={data} />
               </div>
             </div>
           </div>
         )}
         <div className="mt-2">
           <div className="bg-blue-500 px-2 mb-1 rounded text-white">
-            Customer contact and Competitor details
+            influncer details
           </div>
-          <LeadDetails />
+          <InfluncerDetails />
         </div>
       </div>
     </div>
   );
 };
 
-export default Lead;
+export default Qualifying;
